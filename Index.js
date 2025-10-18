698d95425f1b8f0dd74fcdeef77298b0916f7c1a
sricharan12-hub <sricharanpalem07@gmail.com>
2025-10-18 16:06:47 +0530

    corserror

--- Index.js
+++ Index.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
     next();
 });
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: 'https://mern-frontend-6a7t.vercel.app' || "http://localhost:5173",
     credentials: true
 }));
 
